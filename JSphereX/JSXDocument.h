a6d48f72d10b7a807e1609c535ee684d45637ee7
Jos Kuijpers <jos@kuijpersvof.nl>
2013-08-07 00:57:06 +0200

    Initial Commit

---
+++ JSphereX/JSXDocument.h
@@ -0,0 +1,13 @@
+//
+//  JSXDocument.h
+//  JSphereX
+//
+//  Created by Jos Kuijpers on 8/7/13.
+//  Copyright (c) 2013 Jarvix. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JSXDocument : NSDocument
+
+@end
---
+++ JSphereXTests/JSphereXTests.m
@@ -0,0 +1,36 @@
+//
+//  JSphereXTests.m
+//  JSphereXTests
+//
+//  Created by Jos Kuijpers on 8/7/13.
+//  Copyright (c) 2013 Jarvix. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSphereXTests : XCTestCase
+
+@end
+
+@implementation JSphereXTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
