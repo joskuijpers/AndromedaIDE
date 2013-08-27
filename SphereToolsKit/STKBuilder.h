e2f26322d3de0634448891e88eb032222c16ab52
Jos Kuijpers <jos@kuijpersvof.nl>
2013-08-27 16:20:51 +0200

    Complete build setup for Runtime and ToolsKit

---
+++ SphereToolsKit/STKBuilder.h
@@ -0,0 +1,13 @@
+//
+//  STKBuilder.h
+//  Sphere
+//
+//  Created by Jos Kuijpers on 8/27/13.
+//  Copyright (c) 2013 Jarvix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STKBuilder : NSObject
+
+@end
---
+++ SphereToolsKit/STKBuilder.m
@@ -0,0 +1,13 @@
+//
+//  STKBuilder.m
+//  Sphere
+//
+//  Created by Jos Kuijpers on 8/27/13.
+//  Copyright (c) 2013 Jarvix. All rights reserved.
+//
+
+#import "STKBuilder.h"
+
+@implementation STKBuilder
+
+@end
