b13dec93cfde0a66f4ad0d557502e01ae9a49838
Jos Kuijpers <jos@kuijpersvof.nl>
2013-09-17 19:51:07 +0200

    Add more tests. Disable analyzing on every build

--- L8FrameworkTests/L8ValueTests.m
+++ L8FrameworkTests/L8ValueTests.m
@@ -9,22 +9,51 @@
 #import <XCTest/XCTest.h>
 #import "L8Runtime+L8Test.h"
 #import "L8Value.h"
+#import "L8Export.h"
 
-@interface L8ValueTests : XCTestCase
+@interface L8ValueTests : XCTestCase @end
+@interface CustomSimpleObject : NSObject @end
 
+@interface CustomMethodClass : NSObject
+- (void)simpleMethod;
+- (int)methodReturningInteger;
+- (id)methodReturningId;
+- (void)methodWithStringArgument:(NSString *)argument;
 @end
+
+@interface CustomPropertiesClass : NSObject
+@property NSString *stringVal;
+@property double doubleVal;
+@end
+
+@interface CustomPropertiesClassWithAttributes : NSObject
+@property (strong,readonly) NSString *stringVal;
+@property (assign,getter=isHidden) BOOL hidden;
+@property (strong,setter=setMaker:,getter=seeMaker) NSString *maker;
+@property (strong,setter=oddSetter:) NSString *oddities;
+@end
+
+@interface CustomClass : NSObject
+@end
+
 
 @implementation L8ValueTests
 
 - (void)testStringValue
 {
 	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
-		XCTAssertEqualObjects([[L8Value valueWithObject:@"Hello World"] toString], @"Hello World", "String value storage");
+		L8Value *value = [L8Value valueWithObject:@"Hello World"];
 
-		runtime[@"string1"] = @"Hello World";
-		XCTAssertEqualObjects([runtime[@"string1"] toString], @"Hello World", "Value from object");
+		XCTAssertNotNil(value, "-[valueWithObject:]");
+		XCTAssertEqualObjects([value toObject], @"Hello World", "-[toObject]");
+		XCTAssertEqualObjects([value toString], @"Hello World", "-[toString]");
+		XCTAssertTrue([value isString], "-[isString]");
 
-		L8Value *retVal = [runtime evaluateScript:@"string1" withName:@"test.js"];
+
+		runtime[@"string"] = @"Hello World";
+		XCTAssertEqualObjects([runtime[@"string"] toString], @"Hello World", "Value from object");
+
+		L8Value *retVal = [runtime evaluateScript:@"string" withName:@"test.js"];
 		XCTAssertEqualObjects([retVal toString], @"Hello World", "Global string assignment script result");
 	}];
 }
@@ -32,17 +61,289 @@
 - (void)testBooleanValue
 {
 	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
-		XCTAssertEqual([[L8Value valueWithBool:YES] toBool], YES, "Boolean value storage");
+		L8Value *value = [L8Value valueWithBool:YES];
+
+		XCTAssertNotNil(value, "-[valueWithBool:]");
+		XCTAssertEqual([value toBool], YES, "-[toBool]");
+		XCTAssertEqualObjects([value toObject], @YES, "-[toObject]");
+		XCTAssertTrue([value isBoolean], "-[isBool]");
+
+		XCTAssertEqual([value toBool], [[L8Value valueWithObject:@YES] toBool], "-[valueWithObject:]");
+	}];
+}
+
+- (void)testNullValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithNull];
+
+		XCTAssertNotNil(value, "-[valueWithNull]");
+		XCTAssertEqualObjects([value toObject], [NSNull null], "-[toObject]");
+		XCTAssertTrue([value isNull], "-[isNull]");
+
+		XCTAssertEqualObjects([value toObject], [[L8Value valueWithObject:[NSNull null]] toObject], "-[valueWithObject:]");
+	}];
+}
+
+- (void)testUndefinedValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithUndefined];
+
+		XCTAssertNotNil(value, "-[valueWithUndefined]");
+		XCTAssertNil([value toObject], "-[toObject]");
+		XCTAssertTrue([value isUndefined], "-[isUndefined]");
+
+		XCTAssertEqualObjects([value toObject], [[L8Value valueWithObject:nil] toObject], "-[valueWithObject:]");
+	}];
+}
+
+- (void)testDateValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		NSDate *date = [NSDate date];
+		L8Value *value = [L8Value valueWithObject:date];
+
+		XCTAssertNotNil(value, "-[valuewithObject:]");
+		XCTAssertEqual((long long)[value toDouble],(long long)[date timeIntervalSince1970],"-[toDouble]");
+		XCTAssertNotNil([value toObject], "-[toObject]");
+		XCTAssertTrue([value isObject], "-[isObject]");
 	}];
 }
 
 - (void)testDoubleValue
 {
 	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
-		XCTAssertEqualObjects([[L8Value valueWithDouble:1.0] toNumber], @1.0, "Double value storage");
+		L8Value *value = [L8Value valueWithDouble:5.0];
+
+		XCTAssertNotNil(value, "-[valueWithDouble:]");
+		XCTAssertEqualObjects([value toNumber], @5.0, "-[toNumber]");
+		XCTAssertEqualObjects([value toObject], @5.0, "-[toObject]");
+		XCTAssertTrue([value isNumber], "-[isNumber]");
+
+		XCTAssertEqualObjects([value toNumber], [[L8Value valueWithObject:@5.0] toNumber], "-[valueWithObject:]");
 	}];
 }
 
+- (void)testArrayValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithObject:@[@"a",@"b"]];
 
+		XCTAssertNotNil(value, "-[valueWithObject:(NSArray)]");
+		XCTAssertEqualObjects([value toArray], (@[@"a",@"b"]), "-[toArray]");
+		XCTAssertEqualObjects([value toObject], (@[@"a",@"b"]), "-[toObject]");
+		XCTAssertTrue([value isObject], "-[isObject]");
+
+		value = [L8Value valueWithNewArray];
+		XCTAssertNotNil(value, "-[valueWithNewArray]");
+		XCTAssertEqualObjects([value toArray], @[], "-[toArray] (newArray)");
+		XCTAssertEqualObjects([value toObject], @[], "-[toObject] (newArray)");
+		XCTAssertTrue([value isObject], "-[isObject] (newArray)");
+	}];
+}
+
+- (void)testDictionaryValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithObject:@{@"a":@"1",@"b":@2}];
+
+		XCTAssertNotNil(value, "-[valueWithObject:(NSDictionary)]");
+		XCTAssertEqualObjects([value toDictionary], (@{@"a":@"1",@"b":@2}), "-[toArray]");
+		XCTAssertEqualObjects([value toObject], (@{@"a":@"1",@"b":@2}), "-[toObject]");
+		XCTAssertTrue([value isObject], "-[isObject]");
+	}];
+}
+
+- (void)testObjectValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithNewObject];
+
+		XCTAssertNotNil(value, "-[valueWithNewObject]");
+		XCTAssertNotNil([value toObject], "-[toObject]");
+		NSLog(@"OBJECT %@",value);
+		XCTAssertTrue([value isObject], "-[isObject]");
+	}];
+}
+
+- (void)testCustomClassValue
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		id object = [[CustomSimpleObject alloc] init];
+		L8Value *value = [L8Value valueWithObject:object];
+
+		XCTAssertNotNil(value, "-[valueWithObject:(Custom)]");
+		XCTAssertEqual([value toObject], object, "-[toObject]");
+		XCTAssertTrue([value isObject], "-[isObject]");
+	}];
+}
+
+- (void)testProperties
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithNewObject];
+
+		XCTAssertNotNil(value, "-[valueWithNewObject]");
+
+		[value setValue:@"Hello World" forProperty:@"key"];
+		XCTAssertNotNil([value valueForProperty:@"key"],"-[setValue:forProperty:]");
+		XCTAssertEqualObjects([[value valueForProperty:@"key"] toString], @"Hello World", "-[valueForProperty:]");
+		XCTAssertTrue([value hasProperty:@"key"], "-[hasProperty]");
+
+		runtime[@"object"] = value;
+		L8Value *returnValue = [runtime evaluateScript:@"object.key" withName:@""];
+		XCTAssertEqualObjects([returnValue toString], @"Hello World", "getting value in JavaScript");
+
+		XCTAssertEqualObjects([value toDictionary], (@{@"key":@"Hello World"}), "-[toDictionary] (Object conversion)");
+
+		[value deleteProperty:@"key"];
+		XCTAssertFalse([value hasProperty:@"key"], "-[deleteProperty:]");
+	}];
+}
+
+- (void)testKeyedProperties
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithNewObject];
+
+		XCTAssertNotNil(value, "-[valueWithNewObject]");
+
+		value[@"key"] = @"Hello World";
+		XCTAssertNotNil(value[@"key"],"-[setObject:forKeyedSubscript:]");
+		XCTAssertEqualObjects([value[@"key"] toString], @"Hello World", "-[objectForKeyedSubscript:]");
+		XCTAssertTrue([value hasProperty:@"key"], "-[hasProperty]");
+	}];
+}
+
+- (void)testIndexedProperties
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *value = [L8Value valueWithNewArray];
+
+		XCTAssertNotNil(value, "-[valueWithNewArray]");
+
+		value[0] = @"Hello World";
+		XCTAssertNotNil(value[0],"-[setObject:forIndexedSubscript:]");
+		XCTAssertEqualObjects([value[0] toString], @"Hello World", "-[objectForIndexedSubscript:]");
+		XCTAssertTrue([value hasProperty:@"0"], "-[hasProperty]");
+
+		runtime[@"object"] = value;
+		L8Value *returnValue = [runtime evaluateScript:@"object" withName:@""];
+		XCTAssertEqualObjects([returnValue toArray], @[@"Hello World"], "getting value in JavaScript and conversion to NSArray");
+
+		[value deleteProperty:@"0"];
+		XCTAssertFalse([value hasProperty:@"0"], "-[deleteProperty:]");
+	}];
+}
+
+- (void)testCustomObjectWithMethods
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *retVal;
+		CustomMethodClass *object = [[CustomMethodClass alloc] init];
+		L8Value *value = [L8Value valueWithObject:object];
+
+		XCTAssertNotNil(value, "-[valueWithObject:(CustomMethodClass)]");
+
+		retVal = [value invokeMethod:@"simpleMethod" withArguments:@[]];
+		XCTAssertTrue([retVal isUndefined], "-[invokeMethod:(void returning) withArguments:@[]], returns Undefined");
+
+		retVal = [value invokeMethod:@"methodReturningInteger" withArguments:@[]];
+		XCTAssertEqualObjects([retVal toNumber], @42, "-[invokeMethod:(int returning) withArguments:@[]]");
+		XCTAssertTrue([retVal isNumber], "-[retVal isNumber]");
+
+		// TODO: more methods
+	}];
+}
+
+- (void)testCustomObjectWithProperties
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *retVal;
+		CustomPropertiesClass *object = [[CustomPropertiesClass alloc] init];
+		object.stringVal = @"Hello World";
+
+		L8Value *value = [L8Value valueWithObject:object];
+
+		XCTAssertNotNil(value, "-[valueWithObject:(CustomPropertiesClass)]");
+
+		runtime[@"object"] = object;
+		retVal = [runtime evaluateScript:@"object.stringVal" withName:@""];
+		XCTAssertEqualObjects([retVal toString], @"Hello World", "Property getting in JavaScript");
+
+		[runtime evaluateScript:@"object.stringVal = 'John';" withName:@""];
+		XCTAssertEqualObjects(object.stringVal, @"John", "Property setting in JavaScript");
+
+		retVal = [runtime evaluateScript:@"object.notThere" withName:@""];
+		XCTAssertTrue([retVal isUndefined], "Invalid property getting in JavaScript");
+
+		// TODO: more properties
+	}];
+}
+
+- (void)testCustomObjectWithAttributedProperties
+{
+	/*[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		L8Value *retVal;
+		CustomMethodClass *object = [[CustomMethodClass alloc] init];
+		L8Value *value = [L8Value valueWithObject:object];
+
+		XCTAssertNotNil(value, "-[valueWithObject:(CustomMethodClass)]");
+
+		retVal = [value invokeMethod:@"simpleMethod" withArguments:@[]];
+		XCTAssertTrue([retVal isUndefined], "-[invokeMethod:(void returning) withArguments:@[]], returns Undefined");
+
+		retVal = [value invokeMethod:@"methodReturningInteger" withArguments:@[]];
+		XCTAssertEqualObjects([retVal toNumber], @42, "-[invokeMethod:(int returning) withArguments:@[]]");
+		XCTAssertTrue([retVal isNumber], "-[retVal isNumber]");
+
+		// TODO: more methods
+	}];*/
+}
+
+- (void)testCustomJSFunction
+{
+	[[[L8Runtime alloc] init] runWithBlock:^(L8Runtime *runtime) {
+		Class cls = [CustomClass class];
+		L8Value *value = [L8Value valueWithObject:cls];
+
+		XCTAssertNotNil(value, "-[valueWithObject:(Class)]");
+	}];
+}
+
+@end
+
+@implementation CustomSimpleObject @end
+
+@implementation CustomMethodClass
+
+- (void)simpleMethod
+{
+
+}
+
+- (int)methodReturningInteger
+{
+	return 42;
+}
+
+- (id)methodReturningId
+{
+	return nil;
+}
+
+- (void)methodWithStringArgument:(NSString *)argument
+{
+
+}
+
+@end
+
+@implementation CustomPropertiesClass
+
+@end
+
+@implementation CustomClass
 
 @end
