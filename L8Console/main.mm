e48d4fadb642b1774d3c192514ea4888db92c147
Jos Kuijpers <jos@kuijpersvof.nl>
2013-09-24 00:46:01 +0200

    Fix many bugs in L8Framework. Implement custom objects and classes. Blocks are WIP

---
+++ L8Console/main.mm
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  L8Console
+//
+//  Created by Jos Kuijpers on 9/17/13.
+//  Copyright (c) 2013 Jarvix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
