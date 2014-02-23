e8cba4b4103376d17794ca81e156b3d5d16ca1bb
Jos Kuijpers <jos@kuijpersvof.nl>
2014-02-23 13:57:51 +0100

    Use new prefix for the runtime kit

---
+++ SphereRuntimeKit/SRKGame.m
@@ -0,0 +1,13 @@
+//
+//  SPHGame.m
+//  JSphereX
+//
+//  Created by Jos Kuijpers on 8/13/13.
+//  Copyright (c) 2013 Jarvix. All rights reserved.
+//
+
+#import "SRKGame.h"
+
+@implementation SRKGame
+
+@end
